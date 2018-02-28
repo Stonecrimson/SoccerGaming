e3be99ab083abb8a973726c8e88c92464c812727
Ben Stewart <benjamin.r.stewart95@gmail.com>
2018-02-27 23:05:43 -0700

    updated data logger to read correctly

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,53 +14,79 @@ namespace DataLogger
     {
         static Stopwatch timer; 
         static int counter = 0;
+        static int totalCounter = 0; 
         static string file;
         static string testNumber;
-        static string hits;
+        static string[] hits;
         static int trials;
-        static long time; 
+        static long[] times;
+        static string[] strings; static int finalCount; 
+        //static long time; 
         static void Main(string[] args)
         {
+            
             timer = new Stopwatch(); 
             Console.WriteLine("Filename?");
             testNumber = Console.ReadLine();
             Console.WriteLine("Number of Trials?");
             int.TryParse(Console.ReadLine(), out trials);
-            hits = "";  
+            hits = new string[10*trials];
+            times = new long[10*trials]; strings = new string[10*trials]; 
             timer.Start(); 
                 SerialPort sp = new SerialPort("COM3", 115200);
 
                 sp.Open();
             sp.DiscardInBuffer(); 
-            sp.DataReceived += Sp_DataReceived;
+            sp.DataReceived += Sp_TargetDataReceived;
              
 
 
             Console.WriteLine("Press any key to write to file and exit, do not exit until tests are complete");
+            sp.DiscardInBuffer();
+
             Console.ReadKey();
 
             string filepath = @"C:\Users\Public\TestResults\" + testNumber + ".csv";
-            char[] array =hits.ToCharArray();
-            System.IO.File.WriteAllText(filepath, hits); 
-
+            for (int i = 0; i < counter - 1; i++)
+            {
+                string temp = "";
+                char[] split = { ',', ' ' };
+                string[] toks = (hits[i]).Split(split);
+                if (toks.Length == 3)
+                {
+                    temp = times[i].ToString() + "," + toks[0] + "," + toks[1];
+                    strings[i] = temp;
+                }
+            }
+            System.IO.File.WriteAllLines(filepath, strings);  
+              
             timer.Stop(); 
         }
 
-        private static void Sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        private static void Sp_TargetDataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-         
-            if(counter == trials)
+            
+            
+            if (totalCounter >= trials)
             {
-                Console.WriteLine("Test Complete"); 
-                return; 
+                Console.WriteLine("Test Complete");
+                return;
             }
-            Thread.Sleep(5); 
-            SerialPort sp = (SerialPort)sender;
-            while (sp.BytesToRead > 0)
+            times[counter] = timer.ElapsedMilliseconds;
+            long diff = 0; 
+            if (counter > 0)
             {
-                hits += sp.ReadExisting(); 
+              diff = times[counter] - times[counter - 1];
             }
-
+            
+            Console.WriteLine("HIT");
+           //Thread.Sleep(5);
+           SerialPort sp = (SerialPort)sender;
+           hits[counter] = sp.ReadLine();
+           counter++; 
+            //timer.Restart(); 
+            if(diff > 300)
+                totalCounter++;
         }
     }
 }
